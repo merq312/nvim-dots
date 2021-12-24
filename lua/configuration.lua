b4c42355903a74dbcbf7f2c5e856ae16c4b87613
Chamila Gunasena <merc312@gmail.com>
2021-12-24 16:46:47 -0500

    fix null-ls and nvim-ts-utils setup

--- lua/configuration.lua
+++ lua/configuration.lua
@@ -1,229 +1,232 @@
-local nvim_lsp = require'lspconfig'
-local coq = require('coq')
-local servers = { 'rust_analyzer', 'ccls' }
+local nvim_lsp = require("lspconfig")
+local coq = require("coq")
+local servers = { "rust_analyzer", "ccls", "tsserver" }
 
 -- Enable additional lspsevers on Linux
-if vim.loop.os_uname().sysname == 'Linux' then
-  table.insert(servers, 'intelephense')
-  table.insert(servers, 'tailwindcss')
+if vim.loop.os_uname().sysname == "Linux" then
+	table.insert(servers, "intelephense")
+	table.insert(servers, "tailwindcss")
 end
 
 for _, lsp in ipairs(servers) do
-  nvim_lsp[lsp].setup {coq.lsp_ensure_capabilities()}
+	nvim_lsp[lsp].setup({ coq.lsp_ensure_capabilities() })
 end
 
--- nvim-lsp-ts-utils configuration
-require("null-ls").config {}
-require("lspconfig")["null-ls"].setup {}
+require("null-ls").setup({
+	sources = {
+		require("null-ls").builtins.formatting.stylua,
+		require("null-ls").builtins.diagnostics.eslint.with({
+			only_local = "node_modules/.bin",
+		}),
+		require("null-ls").builtins.code_actions.eslint.with({
+			only_local = "node_modules/.bin",
+		}),
+		require("null-ls").builtins.formatting.prettier.with({
+			prefer_local = "node_modules/.bin",
+		}),
+	},
+})
 
-nvim_lsp.tsserver.setup {
-  on_attach = function(client, bufnr)
-    -- disable tsserver formatting if you plan on formatting via null-ls
-    client.resolved_capabilities.document_formatting = false
-    client.resolved_capabilities.document_range_formatting = false
+nvim_lsp.tsserver.setup({
+	-- Needed for inlayHints. Merge this table with your settings or copy
+	-- it from the source if you want to add your own init_options.
+	init_options = require("nvim-lsp-ts-utils").init_options,
+	--
+	on_attach = function(client, bufnr)
+		local ts_utils = require("nvim-lsp-ts-utils")
 
-    local ts_utils = require("nvim-lsp-ts-utils")
+		-- defaults
+		ts_utils.setup({
+			debug = false,
+			disable_commands = false,
+			enable_import_on_completion = false,
 
-    -- defaults
-    ts_utils.setup {
-      debug = false,
-      disable_commands = false,
-      enable_import_on_completion = false,
+			-- import all
+			import_all_timeout = 5000, -- ms
+			-- lower numbers = higher priority
+			import_all_priorities = {
+				same_file = 1, -- add to existing import statement
+				local_files = 2, -- git files or files with relative path markers
+				buffer_content = 3, -- loaded buffer content
+				buffers = 4, -- loaded buffer names
+			},
+			import_all_scan_buffers = 100,
+			import_all_select_source = false,
 
-      -- import all
-      import_all_timeout = 5000, -- ms
-      -- lower numbers indicate higher priority
-      import_all_priorities = {
-        same_file = 1, -- add to existing import statement
-        local_files = 2, -- git files or files with relative path markers
-        buffer_content = 3, -- loaded buffer content
-        buffers = 4, -- loaded buffer names
-      },
-      import_all_scan_buffers = 100,
-      import_all_select_source = false,
+			-- filter diagnostics
+			filter_out_diagnostics_by_severity = {},
+			filter_out_diagnostics_by_code = {},
 
-      -- eslint
-      eslint_enable_code_actions = true,
-      eslint_enable_disable_comments = true,
-      eslint_bin = "eslint",
-      eslint_enable_diagnostics = false,
-      eslint_opts = {},
+			-- inlay hints
+			auto_inlay_hints = true,
+			inlay_hints_highlight = "Comment",
 
-      -- formatting
-      enable_formatting = true,
-      formatter = "prettier",
-      formatter_opts = {},
+			-- update imports on file move
+			update_imports_on_move = false,
+			require_confirmation_on_move = false,
+			watch_dir = nil,
+		})
 
-      -- update imports on file move
-      update_imports_on_move = false,
-      require_confirmation_on_move = false,
-      watch_dir = nil,
+		-- required to fix code action ranges and filter diagnostics
+		ts_utils.setup_client(client)
 
-      -- filter diagnostics
-      filter_out_diagnostics_by_severity = {},
-      filter_out_diagnostics_by_code = {},
-    }
+		-- no default maps, so you may want to define some here
+		local opts = { silent = true }
+		vim.api.nvim_buf_set_keymap(bufnr, "n", "gs", ":TSLspOrganize<CR>", opts)
+		vim.api.nvim_buf_set_keymap(bufnr, "n", "gr", ":TSLspRenameFile<CR>", opts)
+		vim.api.nvim_buf_set_keymap(bufnr, "n", "gi", ":TSLspImportAll<CR>", opts)
+	end,
+})
 
-    -- required to fix code action ranges and filter diagnostics
-    ts_utils.setup_client(client)
+require("rust-tools").setup({})
 
-    -- no default maps, so you may want to define some here
-    local opts = { silent = true }
-    vim.api.nvim_buf_set_keymap(bufnr, "n", "gs", ":TSLspOrganize<CR>", opts)
-    vim.api.nvim_buf_set_keymap(bufnr, "n", "gr", ":TSLspRenameFile<CR>", opts)
-    vim.api.nvim_buf_set_keymap(bufnr, "n", "gi", ":TSLspImportAll<CR>", opts)
-  end
-}
+require("nvim-treesitter.configs").setup({
+	highlight = {
+		enable = true,
+	},
+	autopairs = {
+		enable = true,
+	},
+	incremental_selection = {
+		enable = true,
+		keymaps = {
+			init_selection = "gnn",
+			node_incremental = "grn",
+			scope_incremental = "grc",
+			node_decremental = "grm",
+		},
+	},
+	indent = {
+		enable = true,
+	},
+	textobjects = {
+		select = {
+			enable = true,
+			lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
+			keymaps = {
+				-- You can use the capture groups defined in textobjects.scm
+				["af"] = "@function.outer",
+				["if"] = "@function.inner",
+				["ac"] = "@class.outer",
+				["ic"] = "@class.inner",
+			},
+		},
+		move = {
+			enable = true,
+			set_jumps = true, -- whether to set jumps in the jumplist
+			goto_next_start = {
+				["]m"] = "@function.outer",
+				["]]"] = "@class.outer",
+			},
+			goto_next_end = {
+				["]M"] = "@function.outer",
+				["]["] = "@class.outer",
+			},
+			goto_previous_start = {
+				["[m"] = "@function.outer",
+				["[["] = "@class.outer",
+			},
+			goto_previous_end = {
+				["[M"] = "@function.outer",
+				["[]"] = "@class.outer",
+			},
+		},
+	},
+	rainbow = {
+		enable = true,
+		extended_mode = true, -- Also highlight non-bracket delimiters
+		max_file_lines = nil, -- Do not enable for files with more than n lines, int
+		colors = {}, -- table of hex strings
+		termcolors = {}, -- table of colour name strings
+	},
+})
 
-require('rust-tools').setup({})
+require("telescope").setup({
+	defaults = {
+		layout_strategy = "vertical",
+	},
+})
 
-require'nvim-treesitter.configs'.setup{
-  highlight = {
-    enable = true,
-  },
-  autopairs = {
-    enable = true
-  },
-  incremental_selection = {
-    enable = true,
-    keymaps = {
-      init_selection = 'gnn',
-      node_incremental = 'grn',
-      scope_incremental = 'grc',
-      node_decremental = 'grm',
-    },
-  },
-  indent = {
-    enable = true,
-  },
-  textobjects = {
-    select = {
-      enable = true,
-      lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
-      keymaps = {
-        -- You can use the capture groups defined in textobjects.scm
-        ['af'] = '@function.outer',
-        ['if'] = '@function.inner',
-        ['ac'] = '@class.outer',
-        ['ic'] = '@class.inner',
-      },
-    },
-    move = {
-      enable = true,
-      set_jumps = true, -- whether to set jumps in the jumplist
-      goto_next_start = {
-        [']m'] = '@function.outer',
-        [']]'] = '@class.outer',
-      },
-      goto_next_end = {
-        [']M'] = '@function.outer',
-        [']['] = '@class.outer',
-      },
-      goto_previous_start = {
-        ['[m'] = '@function.outer',
-        ['[['] = '@class.outer',
-      },
-      goto_previous_end = {
-        ['[M'] = '@function.outer',
-        ['[]'] = '@class.outer',
-      },
-    },
-  },
-  rainbow = {
-    enable = true,
-    extended_mode = true, -- Also highlight non-bracket delimiters
-    max_file_lines = nil, -- Do not enable for files with more than n lines, int
-    colors = {}, -- table of hex strings
-    termcolors = {} -- table of colour name strings
-  }
-}
+require("bufferline").setup()
 
-require('telescope').setup{
-  defaults = {
-    layout_strategy = "vertical",
-  }
-}
-
-require('bufferline').setup()
-
-require('gitsigns').setup()
+require("gitsigns").setup()
 
 vim.opt.list = true
 vim.opt.listchars:append("eol:↴")
-require("indent_blankline").setup{
-  -- char = "⋅",
-  buftype_exclude = {"terminal"},
-  show_end_of_line = true,
-}
-
-require('kommentary.config').configure_language("default", {
-  prefer_single_line_comments = true,
+require("indent_blankline").setup({
+	-- char = "⋅",
+	buftype_exclude = { "terminal" },
+	show_end_of_line = true,
 })
 
-require'nvim-tree'.setup{
-  auto_close = true,
-  update_focused_file = {
-    enable = true,
-    update_cwd = true,
-  },
-  view = {
-    side = 'right',
-    auto_resize = true,
-  }
-}
+require("kommentary.config").configure_language("default", {
+	prefer_single_line_comments = true,
+})
 
-require'toggleterm'.setup{
-  size = 20,
-  open_mapping = [[<c-p>]],
-  hide_numbers = true,
-  shade_filetypes = {},
-  shade_terminals = true,
-  shading_factor = 1,
-  start_in_insert = true,
-  insert_mappings = true,
-  persist_size = true,
-  direction = 'horizontal',
-  close_on_exit = true,
-  shell = 'wsl.exe',
-}
+require("nvim-tree").setup({
+	auto_close = true,
+	update_focused_file = {
+		enable = true,
+		update_cwd = true,
+	},
+	view = {
+		side = "right",
+		auto_resize = true,
+	},
+})
 
-require('lspkind').init({})
+require("toggleterm").setup({
+	size = 20,
+	open_mapping = [[<c-p>]],
+	hide_numbers = true,
+	shade_filetypes = {},
+	shade_terminals = true,
+	shading_factor = 1,
+	start_in_insert = true,
+	insert_mappings = true,
+	persist_size = true,
+	direction = "horizontal",
+	close_on_exit = true,
+	shell = "wsl.exe",
+})
 
-require('nvim-autopairs').setup({})
+require("lspkind").init({})
+
+require("nvim-autopairs").setup({})
 
 local remap = vim.api.nvim_set_keymap
-local npairs = require('nvim-autopairs')
+local npairs = require("nvim-autopairs")
 
 npairs.setup({ map_bs = false })
 
 -- these mappings are coq recommended mappings unrelated to nvim-autopairs
-remap('i', '<esc>', [[pumvisible() ? "<c-e><esc>" : "<esc>"]], { expr = true, noremap = true })
-remap('i', '<c-c>', [[pumvisible() ? "<c-e><c-c>" : "<c-c>"]], { expr = true, noremap = true })
-remap('i', '<tab>', [[pumvisible() ? "<c-n>" : "<tab>"]], { expr = true, noremap = true })
-remap('i', '<s-tab>', [[pumvisible() ? "<c-p>" : "<bs>"]], { expr = true, noremap = true })
+remap("i", "<esc>", [[pumvisible() ? "<c-e><esc>" : "<esc>"]], { expr = true, noremap = true })
+remap("i", "<c-c>", [[pumvisible() ? "<c-e><c-c>" : "<c-c>"]], { expr = true, noremap = true })
+remap("i", "<tab>", [[pumvisible() ? "<c-n>" : "<tab>"]], { expr = true, noremap = true })
+remap("i", "<s-tab>", [[pumvisible() ? "<c-p>" : "<bs>"]], { expr = true, noremap = true })
 
 -- skip it, if you use another global object
-_G.MUtils= {}
+_G.MUtils = {}
 
 MUtils.CR = function()
-  if vim.fn.pumvisible() ~= 0 then
-    if vim.fn.complete_info({ 'selected' }).selected ~= -1 then
-      return npairs.esc('<c-y>')
-    else
-      -- you can change <c-g><c-g> to <c-e> if you don't use other i_CTRL-X modes
-      return npairs.esc('<c-g><c-g>') .. npairs.autopairs_cr()
-    end
-  else
-    return npairs.autopairs_cr()
-  end
+	if vim.fn.pumvisible() ~= 0 then
+		if vim.fn.complete_info({ "selected" }).selected ~= -1 then
+			return npairs.esc("<c-y>")
+		else
+			-- you can change <c-g><c-g> to <c-e> if you don't use other i_CTRL-X modes
+			return npairs.esc("<c-g><c-g>") .. npairs.autopairs_cr()
+		end
+	else
+		return npairs.autopairs_cr()
+	end
 end
-remap('i', '<cr>', 'v:lua.MUtils.CR()', { expr = true, noremap = true })
+remap("i", "<cr>", "v:lua.MUtils.CR()", { expr = true, noremap = true })
 
 MUtils.BS = function()
-  if vim.fn.pumvisible() ~= 0 and vim.fn.complete_info({ 'mode' }).mode == 'eval' then
-    return npairs.esc('<c-e>') .. npairs.autopairs_bs()
-  else
-    return npairs.autopairs_bs()
-  end
+	if vim.fn.pumvisible() ~= 0 and vim.fn.complete_info({ "mode" }).mode == "eval" then
+		return npairs.esc("<c-e>") .. npairs.autopairs_bs()
+	else
+		return npairs.autopairs_bs()
+	end
 end
-remap('i', '<bs>', 'v:lua.MUtils.BS()', { expr = true, noremap = true })
+remap("i", "<bs>", "v:lua.MUtils.BS()", { expr = true, noremap = true })
