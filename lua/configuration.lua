0f1ea76c46d28a2d94ed8623dde8dbeffcf5d4e6
merq312 <merc312@gmail.com>
2022-03-23 22:18:24 -0500

    move vimwiki to dropbox; remove git blamer (inline git blame)

--- lua/configuration.lua
+++ lua/configuration.lua
@@ -251,8 +251,3 @@ require("kanagawa").setup({
 })
 
 require("todo-comments").setup({})
-
-vim.g.blamer_enabled = 1
-vim.g.blamer_delay = 200
-vim.g.blamer_show_in_visual_modes = 0
-vim.g.blamer_show_in_insert_modes = 0
